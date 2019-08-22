03c984575adcb416d1d3f28b5f738f3f656e7e55
gspayonk <grissobel.castro@gmail.com>
2019-08-22 18:08:08 -0400

    commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,9 +14,7 @@ class Person {
 
 class Instructor extends Person {
     constructor(basicInfo) {
-        this.name = basicInfo.name;
-        this.age = basicInfo.age;
-        this.location = basicInfo.location;
+        super(basicInfo);
         this.specialty = basicInfo.specialty;
         this.favLanguage = basicInfo.favLanguage;
         this.catchPhrase = basicInfo.catchPhrase;
@@ -30,18 +28,152 @@ class Instructor extends Person {
     }
 }
 
-class Student {
+class Student extends Person {
     constructor(basicTwo) {
-        this.name = basicTwo.name;
-        this.age = basicTwo.age;
-        this.location = basicTwo.location;
+        super(basicTwo);
         this.previousBackground = basicTwo.previousBackground;
-
+        this.className = basicTwo.className;
+        this.favSubjects = basicTwo.favSubjects;
+        this.grade = basicTwo.grade;
     }
 
-    speak() {
-        console.log(`
-                    Hello my name is $ { this.name }, I am from $ { this.location }
-                    `)
+    listsSubjects() {
+        console.log(`${this.name}'s favorite subjects are:`);
+        for (var subject of this.favSubjects) {
+            console.log(`${subject}`);
+        }
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+    }
+
+    pass() {
+        if (this.grade >= 65) {
+            console.log(`${this.name} You did it!`);
+        } else {
+            console.log(`Sorry, not passing yet. Remember to review and ask questions. Good Luck!`);
+        }
     }
 }
+
+class ProjectManagers extends Instructor {
+    constructor(evenMoreInfo) {
+        super(evenMoreInfo);
+        this.gradClassName = evenMoreInfo.gradClassName;
+        this.favInstructor = evenMoreInfo.favInstructor;
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`);
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+const alice = new Person({
+    name: 'Alice',
+    location: 'Maine',
+    age: 50,
+});
+
+const harry = new Person({
+    name: 'Harry',
+    location: 'England',
+    age: 30,
+});
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`,
+});
+
+const paolo = new Instructor({
+    name: 'Paolo',
+    location: 'California',
+    age: 21,
+    favLanguage: 'Pythont',
+    specialty: 'Back-End',
+    catchPhrase: `Tubular`,
+});
+
+const hector = new Student({
+    name: 'Hector',
+    location: 'Spain',
+    age: 35,
+    previousBackground: 'Wine Master',
+    className: 'CS 123',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+    grade: 45,
+});
+
+const lucia = new Student({
+    name: 'Lucia',
+    location: 'Portugal',
+    age: 45,
+    previousBackground: 'Doctor',
+    className: 'BIO 333',
+    favSubjects: ['Python', 'C++', 'Java'],
+    grade: 90,
+});
+
+const han = new ProjectManagers({
+    name: 'Han',
+    location: 'Corellia',
+    age: 29,
+    favLanguage: 'Attack',
+    specialty: 'Galaxy Thief',
+    catchPhrase: `You know, sometimes I amaze even myself`,
+    gradClassName: 'CS6',
+    favInstructor: 'Fred',
+});
+
+const tina = new ProjectManagers({
+    name: 'Tina',
+    location: 'New Jersey',
+    age: 25,
+    favLanguage: 'Boys',
+    specialty: 'Writing',
+    catchPhrase: `A No Is Just A Yes, Upside Down`,
+    gradClassName: 'CS5',
+    favInstructor: 'Paolo',
+});
+
+alice.speak();
+harry.speak();
+fred.speak();
+paolo.speak();
+hector.speak();
+lucia.speak();
+han.speak();
+tina.speak();
+han.demo("space");
+tina.demo("home");
+paolo.demo("something");
+fred.demo("code");
+han.grade(hector, "math");
+tina.grade(lucia, "coding");
+paolo.grade(hector, "happy");
+fred.grade(lucia, "upbeat");
+hector.listsSubjects();
+lucia.listsSubjects();
+hector.PRAssignment("class");
+lucia.PRAssignment("lab");
+hector.sprintChallenge("javascript");
+lucia.sprintChallenge("html");
+han.standUp("slack");
+tina.standUp("web20x");
+tina.debugsCode(hector, "standup");
+han.debugsCode(lucia, "project");
+hector.pass();
+lucia.pass();
